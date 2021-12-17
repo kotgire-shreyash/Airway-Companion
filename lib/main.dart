fa9ef60b0483561889cbd1c76dea72e932be4e6d
Ninad Deogaonkar <ninaddeo07@gmail.com>
2021-12-17 19:15:26 +0530

    issue resolver commits

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'Logic/Bloc/AuthenticationBloc/login_bloc.dart';
