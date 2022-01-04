a04dddbf8ca30866f5023362bf94a2c660acacd7
Ninad Deogaonkar <ninaddeo07@gmail.com>
2022-01-04 21:11:10 +0530

    Navigation Screen UI changes

--- lib/Modules/Navigation/Screens/navigation_screen.dart
+++ lib/Modules/Navigation/Screens/navigation_screen.dart
@@ -1,9 +1,8 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:airwaycompanion/Data/Repositories/RoutesRepository/route_data_provider.dart';
 import 'package:airwaycompanion/Data/Repositories/SearchRepository/search_model.dart';
 import 'package:airwaycompanion/Modules/Navigation/Widgets/custom_marker.dart';
 import 'package:airwaycompanion/Modules/Navigation/Widgets/explore_widget.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:vector_map_tiles/vector_map_tiles.dart';
 import 'package:flutter/material.dart';
@@ -23,7 +22,6 @@ class NavigationScreen extends StatefulWidget {
 
 class _NavigationScreenState extends State<NavigationScreen> {
   final MapController _mapController = MapController();
-  //final _intialPostion = LatLng(13.199165, 77.707984);
   final List<Marker> _markers = [];
   final _origin = LatLng(13.199165, 77.707984);
   var points = <LatLng>[];
@@ -34,7 +32,7 @@ class _NavigationScreenState extends State<NavigationScreen> {
       for (var item in searchResultList) {
         _markers.add(CustomMarker(point: _origin, color: Colors.red));
         _markers
-            .add(CustomMarker(point: LatLng(item.Latitude, item.Longitude)));
+            .add(CustomMarker(point: LatLng(item.latitude, item.longitude)));
       }
     });
   }
@@ -42,105 +40,159 @@ class _NavigationScreenState extends State<NavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(
-            Icons.arrow_back,
-            color: Colors.black,
+      body: Stack(
+        children: [
+          SlidingUpPanel(
+            panelBuilder: (sc) => ExploreWidget(
+              scrollController: sc,
+            ),
+            minHeight: 80,
+            body: mapWidget(),
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+            backdropEnabled: false,
           ),
-        ),
-      ),
-      body: SlidingUpPanel(
-        panelBuilder: (sc) => ExploreWidget(
-          scrollController: sc,
-        ),
-        body: mapWidget(),
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(30),
-          topRight: Radius.circular(30),
-        ),
-        backdropEnabled: false,
-      ),
-    );
-  }
-
-  Widget mapWidget() {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: FlutterMap(
-        mapController: _mapController,
-        options: MapOptions(
-          onMapCreated: (mapController) {
-            _markers.add(
-                CustomMarker(point: _origin, color: Colors.redAccent.shade700));
-          },
-          center: _origin,
-          zoom: 10,
-          plugins: [
-            VectorMapTilesPlugin(),
-          ],
-          minZoom: 5.0,
-          maxZoom: 30.0,
-          interactiveFlags: InteractiveFlag.pinchZoom | InteractiveFlag.drag,
-        ),
-        layers: [
-          TileLayerOptions(
-            minZoom: 2,
-            maxZoom: 22,
-            errorTileCallback: (tile, error) => print(error),
-            urlTemplate:
-                "https://atlas.microsoft.com/map/tile?subscription-key={subscriptionKey}&api-version=2.1&tileSize=512&tilesetId={tilesetId}&zoom={z}&x={x}&y={y}",
-            additionalOptions: {
-              'subscriptionKey': 'OmHax9byGsCpudWRxU0lYnTgw81r6Eq9nlCqRk3EnGI',
-              'tilesetId': 'microsoft.base.road'
-            },
-            tileProvider: const NonCachingNetworkTileProvider(),
-            //errorImage: image to be displayed when map not loaded
+          // SafeArea(
+          //   child: IconButton(
+          //     onPressed: () {
+          //       Navigator.pop(context);
+          //     },
+          //     icon: const Icon(
+          //       Icons.arrow_back,
+          //       color: Colors.black,
+          //       size: 28,
+          //     ),
+          //   ),
+          // ),
+          SafeArea(
+            child: Container(
+                alignment: Alignment.topCenter,
+                margin: const EdgeInsets.only(left: 10, right: 10),
+                child: _searchBar()),
           ),
-          // For building Polylines
-          PolylineLayerOptions(
-            polylines: [
-              Polyline(
-                points: points,
-                strokeWidth: 4.0,
-                isDotted: true,
-                colorsStop: [5, 10],
-                color: Colors.blue.shade700,
-              ),
-            ],
-          ),
-          MarkerLayerOptions(
-            markers: _markers,
+          SafeArea(
+            child: Container(
+                alignment: Alignment.topCenter,
+                margin: EdgeInsets.only(
+                    top: 60,
+                    bottom: MediaQuery.of(context).size.height - 150,
+                    left: 5,
+                    right: 5),
+                child: const QuickSelect()),
           ),
         ],
       ),
     );
   }
 
-  void drawPolyline(destination) async {
-    Routes _Routes = Routes(origin: _origin, destination: destination);
-    points = await _Routes.getRouteDetails();
+  Widget mapWidget() {
+    return FlutterMap(
+      mapController: _mapController,
+      options: MapOptions(
+        onMapCreated: (mapController) {
+          _markers.add(
+              CustomMarker(point: _origin, color: Colors.redAccent.shade700));
+        },
+        center: _origin,
+        zoom: 10,
+        plugins: [
+          VectorMapTilesPlugin(),
+        ],
+        minZoom: 5.0,
+        maxZoom: 30.0,
+        interactiveFlags: InteractiveFlag.pinchZoom | InteractiveFlag.drag,
+      ),
+      layers: [
+        TileLayerOptions(
+          minZoom: 2,
+          maxZoom: 22,
+          errorTileCallback: (tile, error) => print(error),
+          urlTemplate:
+              "https://atlas.microsoft.com/map/tile?subscription-key={subscriptionKey}&api-version=2.1&tileSize=512&tilesetId={tilesetId}&zoom={z}&x={x}&y={y}",
+          additionalOptions: {
+            'subscriptionKey': 'OmHax9byGsCpudWRxU0lYnTgw81r6Eq9nlCqRk3EnGI',
+            'tilesetId': 'microsoft.base.road'
+          },
+          tileProvider: const NonCachingNetworkTileProvider(),
+          errorImage: const Image(
+            image: AssetImage("assets/GIF/mao_loading.gif"),
+            fit: BoxFit.scaleDown,
+          ).image,
+          // errorImage: AssetImage("assets/GIF/map_loading.gif"),
+          // errorImage: GIF
+        ),
+        // For building Polylines
+        PolylineLayerOptions(
+          polylines: [
+            Polyline(
+              points: points,
+              strokeWidth: 4.0,
+              isDotted: true,
+              colorsStop: [5, 10],
+              color: Colors.blue.shade700,
+            ),
+          ],
+        ),
+        MarkerLayerOptions(
+          markers: _markers,
+        ),
+      ],
+    );
   }
 
-  Widget serchBar() {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 34.0),
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        child: TextField(
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.all(16.0),
-            hintText: "Looking for something ?",
-            prefixIcon: Icon(Icons.search_outlined),
-          ),
+  void drawPolyline(destination) async {
+    Routes _routes = Routes(origin: _origin, destination: destination);
+    points = await _routes.getRouteDetails();
+  }
+
+  Widget _searchBar() {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: SizedBox(
+        height: 50,
+        width: 350,
+        child: Row(
+          children: [
+            SizedBox(
+              height: 50,
+              width: 50,
+              child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.arrow_back)),
+            ),
+            Flexible(
+              child: SizedBox(
+                height: 50,
+                width: 280,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    isDense: true,
+
+                    contentPadding: const EdgeInsets.all(16.0),
+                    hintText: "Looking for something ?",
+                    hintStyle: TextStyle(
+                        fontFamily: GoogleFonts.lato().fontFamily,
+                        fontSize: 15),
+
+                    // prefixIcon: const Icon(Icons.arrow_back),
+                    suffixIcon: const Icon(
+                      Icons.search_outlined,
+                      size: 25,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+// https://meet.google.com/tfe-uihw-nke
--- lib/Modules/Navigation/Widgets/explore_widget.dart
+++ lib/Modules/Navigation/Widgets/explore_widget.dart
@@ -1,11 +1,31 @@
-// ignore_for_file: unused_field
-
 import 'package:airwaycompanion/Data/Repositories/SearchRepository/search_data_provider.dart';
 import 'package:airwaycompanion/Data/Repositories/SearchRepository/search_model.dart';
 import 'package:airwaycompanion/Modules/Navigation/Screens/navigation_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:group_button/group_button.dart';
+
+final List<String> _buttons = [
+  'Hotels',
+  'Restaurant',
+  'ATM',
+  'Cafe',
+  'pharmacy',
+  'hospital',
+  'Books',
+  'Taxi',
+];
+
+final List _icons = [
+  Icons.hotel,
+  Icons.restaurant,
+  Icons.atm,
+  Icons.local_cafe,
+  Icons.medical_services,
+  Icons.local_hospital,
+  Icons.library_books,
+  Icons.local_taxi,
+];
 
 class ExploreWidget extends StatefulWidget {
   const ExploreWidget({Key? key, required this.scrollController})
@@ -21,59 +41,48 @@ class _ExploreWidgetState extends State<ExploreWidget> {
       GoogleFonts.lato(fontWeight: FontWeight.w900).fontFamily;
   final Search _search = Search();
   List<SearchModel> searchResultList = [];
+  int iconIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       controller: widget.scrollController,
       physics: const BouncingScrollPhysics(),
       children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 15),
-          child: Text(
-            "Explore",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.grey.shade700,
-                fontSize: 15,
-                fontFamily: _latoBoldFontFamily,
-                fontWeight: FontWeight.w900),
-            textScaleFactor: 1.6,
-          ),
+        Text(
+          "Explore nearby",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Colors.grey.shade700,
+              fontSize: 15,
+              fontFamily: _latoBoldFontFamily,
+              fontWeight: FontWeight.w900),
+          textScaleFactor: 1.6,
         ),
         const SizedBox(
           height: 10,
         ),
-        optionslist(),
-        SizedBox(
+        _getOptionsList(),
+        const SizedBox(
           height: 10,
         ),
-        searchResultList.isEmpty ? Container() : itemlist()
+        searchResultList.isEmpty ? Container() : _getItemsList()
       ],
     );
   }
 
-  optionslist() {
-    List<String> _buttons = [
-      'Hotels',
-      'Restaurant',
-      'ATM',
-      'Cafe',
-      'pharmacy',
-      'hospital',
-      'Books',
-      'Taxi',
-    ];
+  _getOptionsList() {
     return GroupButton(
       isRadio: false,
       spacing: 10,
       onSelected: (index, isSelected) async {
+        searchResultList.clear();
+        NavigationScreen.of(context)!.points.clear();
         if (isSelected) {
           searchResultList.clear();
           searchResultList =
               await _search.searchNearby(searchdata: _buttons[index]);
-        } else {
-          searchResultList.clear();
-          NavigationScreen.of(context)!.points.clear();
+          iconIndex = index;
         }
         NavigationScreen.of(context)!.result = searchResultList;
       },
@@ -82,22 +91,71 @@ class _ExploreWidgetState extends State<ExploreWidget> {
     );
   }
 
-  itemlist() {
+  _getItemsList() {
+    final ScrollController _listViewScrollController = ScrollController();
     return ListView.builder(
       shrinkWrap: true,
+      controller: _listViewScrollController,
       itemCount: searchResultList.length,
-      physics: ScrollPhysics(),
+      physics: const BouncingScrollPhysics(),
       scrollDirection: Axis.vertical,
       itemBuilder: (context, index) {
         return ListTile(
-          leading: Icon(Icons.restaurant),
-          title: Text(searchResultList[index].Name),
-          subtitle: Text(searchResultList[index].Address),
+          leading: Icon(_icons[iconIndex]),
+          title: Text(searchResultList[index].name),
+          subtitle: Text(searchResultList[index].address),
           trailing: Text(
             '${searchResultList[index].distance.toStringAsFixed(2)}m',
-            style: TextStyle(color: Colors.blue),
+            style: const TextStyle(color: Colors.blue),
           ),
           isThreeLine: true,
+        );
+      },
+    );
+  }
+}
+
+class QuickSelect extends StatelessWidget {
+  const QuickSelect({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      physics: const BouncingScrollPhysics(),
+      itemCount: _buttons.length,
+      itemBuilder: (context, int index) {
+        return Row(
+          children: [
+            Card(
+              child: Container(
+                height: 50,
+                width: 120,
+                child: FloatingActionButton(
+                  elevation: 0,
+                  backgroundColor: Colors.white,
+                  isExtended: true,
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(
+                        _icons[index],
+                        color: Colors.black,
+                      ),
+                      Text(
+                        _buttons[index],
+                        style: const TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 5,
+            )
+          ],
         );
       },
     );
